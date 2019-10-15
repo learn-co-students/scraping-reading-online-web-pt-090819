70d536eaf28cf82808d7382f718c7ac1aba24756
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-15 20:56:38 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com"))
+puts element = doc.css(".headline-26OIBN")
+binding.pry
